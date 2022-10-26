6913f7c3c9773a5159f02c5304d4cd22bd4d9d71
Sofia <79449314+sofiapfund@users.noreply.github.com>
2022-10-26 13:58:08 +0200

    Update script.py

--- script.py
+++ script.py
@@ -30,90 +30,38 @@ lymphoma = pd.read_csv("data/lymphoma.csv")
 
 ####################### Step 3: data pre-processing ###########################
 
-# find lymphoma: ?????????
+### 1. match lymphoma samples to CNV samples that exhibit mutations in the 4 genes of interest:
 
-# create new datasets by matching "sample_id" column from lymphoma with "id" column from group_info:
-tp53_df = pd.merge(lymphoma, tp_53_group_info, left_on = "id", right_on = "biosample_id") # filter for rows where "biosample_id" matches the "id"
+# match "sample_id" column from lymphoma with "id" column from group_info:
+tp53_df = pd.merge(lymphoma, tp_53_group_info, left_on = "id", right_on = "biosample_id")
 cdkn2a_df = pd.merge(lymphoma, cdkn2a_group_info, left_on = "id", right_on = "biosample_id")
 myc_df = pd.merge(lymphoma, myc_group_info, left_on = "id", right_on = "biosample_id")
 erbb2_df = pd.merge(lymphoma, erbb2_group_info, left_on = "id", right_on = "biosample_id")
 
-# add column with label for later classification:
+# add column with label for later grouping:
 tp53_df["group"] = "tp53" # lymphoma tumor samples that have mutations in the tp53 gene
 cdkn2a_df["group"] = "cdkn2a"
 myc_df["group"] = "myc"
 erbb2_df["group"] = "erbb2"
 
-
+# merge everything into the same dataset:
 whole_df = pd.concat([tp53_df, cdkn2a_df, myc_df, erbb2_df])
-#print(whole_dataset.shape)
+  
+### 2. select features of interest:
+whole_df = whole_df[['info.followupMonths',             # how much time has passed since the sample was collected 
+                     'info.death',                      # is dead or alive at time of sample collection? (0, 1)
+                     'group',                           # which gene mutation in the tumor sample? (tp53, myc, ...)
+                     'sex',                             # sex of the patient (male, female)
+                     'histologicalDiagnosis.id',        # name of the tumor type
+                     'pathologicalStage.label'          # cancer stage of the sample
+                     'info.cnvstatistics.cnvfraction'   # CNV fraction in the whole genome of the sample
+                    ]]
 
-# select features of interest:
-whole_df = whole_df[['info.followupMonths', 'info.death', 'group', 'histologicalDiagnosis.id',
-                                'info.cnvstatistics.cnvfraction', 'sex', 'pathologicalStage.label',
-                                'info.cnvstatistics.dupfraction',
-                                'info.cnvstatistics.delfraction']]
-
-# remove NaN:
-whole_dataset = whole_dataset.dropna()
-
-# get info about the dataset:
-whole_df.info()
-
-######### KM plots ##############################################################
-
-# Compute Survival based on gene
-time = whole_dataset["info.followupMonths"]  # how much time has passed since the sample was collected
-event = whole_dataset["info.death"]
-group = whole_dataset["group"]
-results = km.fit(time, event, group)
-# Plot
-km.plot(results)
-plt.show()
-
-################################################################################
-
-# Compute Survival based on sex
-time = whole_dataset["info.followupMonths"]  # how much time has passed since the sample was collected
-event = whole_dataset["info.death"]
-sex = whole_dataset["sex"]
-results_2 = km.fit(time, event, sex)
-# Plot
-km.plot(results_2)
-plt.show()
-
-################################################################################
-
-# Frequencies of CNVs samples for each gene of interest
-plt.figure(figsize=(8,5))
-sns.countplot(x='group', data=whole_dataset, palette='rainbow')
-plt.title("Nr of CNVs Samples by Gene in the `lymphoma.cvs` dataset")
-plt.xlabel("Gene")
-plt.ylabel("Count")
-plt.show()
-
-################################################################################
-
-## CNV fraction by tumor stage: does CNV fraction increases by tumor stage?
-
-# sort the dataset by stage
-whole_dataset = whole_dataset.sort_values(['pathologicalStage.label'])
-
-# plot
-plt.figure(figsize=(20,6))
-sns.violinplot(x='pathologicalStage.label',y="info.cnvstatistics.cnvfraction", data=whole_dataset)
-plt.title("Violin Plot of CNV fraction based on Tumor Stage")
-plt.xlabel("Tumor stage")
-plt.ylabel("CNV fraction") # what fraction of the TP53 gene has CNV (statistics for all samples of a specific malignancy)
-plt.show()
-
-################################################################################
-
-# filter the data:
+### 3. select rows (tumors) of interest:
 ncit_codes = ["NCIT:C80280", "NCIT:C4340", "NCIT:C3246", "NCIT:C4337", "NCIT:C27753", "NCIT:C8851"]
-names = ["Diffuse Large B-Cell Lymphoma", "Peripheral T-Cell Lymphoma", "Mycosis Fungoides", "Mantle Cell Lymphoma", "Acute Myeloid Leukemia", "Diffuse Large B-Cell Lymphoma"]
-filtered_data = whole_dataset[whole_dataset['histologicalDiagnosis.id'].isin(ncit_codes)]
+filtered_df = whole_df[whole_df['histologicalDiagnosis.id'].isin(ncit_codes)]
 
+### 4. replace NCIT tumor codes with human-readable tumor names:
 def my_func(row):
     if row['histologicalDiagnosis.id'] == "NCIT:C80280":
         val = 'Diffuse Large B-Cell Lymphoma'
@@ -129,19 +77,61 @@ def my_func(row):
         val = 'Diffuse Large B-Cell Lymphoma'
     return val
 
-filtered_data['name'] = filtered_data.apply(my_func, axis=1)
+filtered_df['name'] = filtered_df.apply(my_func, axis=1)
 
-####################### Plotting ###############################################
+### 5. remove missing values:
+filtered_df = filtered_df.dropna()
 
-## KM-curve based on tumor type
-time = filtered_data["info.followupMonths"]  # how much time has passed since the sample was collected
-event = filtered_data["info.death"]
-tumor = filtered_data["name"]
-results_3 = km.fit(time, event, tumor)
+### 6. visually inspect data: frequencies of CNVs samples for each gene of interest
+plt.figure(figsize=(8,5))
+sns.countplot(x='group', data=filtered_df, palette='rainbow')
+plt.title("Nr of CNVs Samples by Gene in the `lymphoma.cvs` dataset")
+plt.xlabel("Gene")
+plt.ylabel("Count")
+plt.show()
+
+####################### Step 4: survival analysis ##############################
+
+# compute survival based on gene mutation:
+time = filtered_df["info.followupMonths"]
+event = filtered_df["info.death"]
+group = filtered_df["group"]
+results = km.fit(time, event, group)
+km.plot(results) # visualize Kaplan-Meier plot
+plt.show()
+
+# compute survival based on sex:
+time = filtered_df["info.followupMonths"]
+event = filtered_df["info.death"]
+sex = filtered_df["sex"]
+results_2 = km.fit(time, event, sex)
+km.plot(results_2) # visualize Kaplan-Meier plot
+plt.show()
+
+# compute survival based on tumor type:
+time = filtered_df["info.followupMonths"]
+event = filtered_df["info.death"]
+tumor = filtered_df["name"]
+results_3 = km.fit(time, event, tumor) # visualize Kaplan-Meier plot
 km.plot(results_3)
 plt.show()
 
-## CNV fraction by tumor type (violin plot)
+####################### Step 5: exploratory analysis #########################
+
+### Q1: Does CNV fraction change depending on tumor stage?
+
+# sort the dataset by stage
+filtered_df = filtered_df.sort_values(['pathologicalStage.label'])
+
+# plot
+plt.figure(figsize=(20,6))
+sns.violinplot(x='pathologicalStage.label', y="info.cnvstatistics.cnvfraction", data=filtered_df)
+plt.title("Violin Plot of CNV fraction based on Tumor Stage")
+plt.xlabel("Tumor stage")
+plt.ylabel("CNV fraction")
+plt.show()
+
+### Q2: Does CNV fraction change depending on tumor type?
 plt.figure(figsize=(15,6))
 sns.violinplot(x='name',y="info.cnvstatistics.cnvfraction", data=filtered_data, palette='rainbow')
 plt.title("Violin Plot of CNV fraction by Tumor Type")
@@ -149,7 +139,7 @@ plt.xlabel("Tumor Type")
 plt.ylabel("CNV fraction")
 plt.show()
 
-## CNV fraction by gene (violin plot)
+## Q3: Does CNV fraction change depending on mutated gene? 
 plt.figure(figsize=(10,6))
 sns.violinplot(x='group',y="info.cnvstatistics.cnvfraction", data=filtered_data, palette='rainbow')
 plt.title("Violin Plot of CNV fraction vs. Mutated Gene")
@@ -157,90 +147,6 @@ plt.xlabel("Mutated Gene")
 plt.ylabel("CNV fraction")
 plt.show()
 
-plt.figure(figsize=(10,6))
-sns.violinplot(x='group',y="info.cnvstatistics.dupfraction", data=filtered_data, palette='rainbow')
-plt.title("Violin Plot of CNV duplication fraction vs. Mutated Gene")
-plt.xlabel("Mutated Gene")
-plt.ylabel("CNV duplication fraction")
-plt.show()
-
-plt.figure(figsize=(10,6))
-sns.violinplot(x='group',y="info.cnvstatistics.delfraction", data=filtered_data, palette='rainbow')
-plt.title("Violin Plot of CNV deletion fraction vs. Mutated Gene")
-plt.xlabel("Mutated Gene")
-plt.ylabel("CNV deletion fraction")
-plt.show()
-
-###################### Additional Code #########################################
-
-#plt.figure(figsize=(20,6))
-#sns.violinplot(x='histologicalDiagnosis.id',y="info.cnvstatistics.cnvfraction",data=whole_dataset, palette='rainbow')
-#plt.title("Violin Plot of CNV fraction by Tumor Type")
-#plt.xlabel("NCIT code of the Tumor")
-#plt.show()
-
-#plt.figure(figsize=(10,6))
-#sns.violinplot(x='histologicalDiagnosis.id',y="info.cnvstatistics.cnvfraction",data=whole_dataset, hue='group', palette='rainbow')
-#plt.title("Violin Plot of CNV distribution by Tumor Type, Separated by Gene")
-#plt.xlabel("NCIT code of the malignancy")
-#plt.show()
-
-
 #############################################################################
 #############################################################################
 #############################################################################
-
-######### matching ids ####################
-# read the sample datasets
-
-# gene data
-tp_53_group_info = pd.read_csv("biosample-tp53.tsv", sep="\t")
-cdkn2a_group_info = pd.read_csv("biosample-CDKN2A.tsv", sep="\t")
-myc_group_info = pd.read_csv("biosample-myc.tsv", sep="\t")
-erbb2_group_info = pd.read_csv("biosample-ERBB2.tsv", sep="\t")
-
-# tumor data
-dataset = pd.read_csv("lymphoma.csv")
-
-# match columns "sample_id" in dataset and columns "id" in group_info
-# => filter for rows where "biosample_id" matches the "id"
-tp53_dataset = pd.merge(dataset, tp_53_group_info, left_on = "id", right_on = "biosample_id")
-tp53_dataset["group"] = "tp53"
-#print(tp53_dataset)
-
-cdkn2a_dataset = pd.merge(dataset, cdkn2a_group_info, left_on = "id", right_on = "biosample_id")
-cdkn2a_dataset["group"] = "cdkn2a"
-#print(cdkn2a_dataset)
-
-myc_dataset = pd.merge(dataset, myc_group_info, left_on = "id", right_on = "biosample_id")
-myc_dataset["group"] = "myc"
-#print(myc_dataset)
-
-erbb2_dataset = pd.merge(dataset, erbb2_group_info, left_on = "id", right_on = "biosample_id")
-erbb2_dataset["group"] = "erbb2"
-#print(erbb2_dataset)
-
-whole_dataset = pd.concat([tp53_dataset, cdkn2a_dataset, myc_dataset, erbb2_dataset])
-print(whole_dataset.shape)
-
-whole_dataset = whole_dataset[['info.followupMonths', 'info.death', 'group']]
-
-whole_dataset = whole_dataset.dropna()
-print(whole_dataset.shape)
-
-######### KM plot ########################
-# Compute Survival
-time = whole_dataset["info.followupMonths"]  # how much time has passed since the sample was collected
-event = whole_dataset["info.death"]
-group = whole_dataset["group"]
-results = km.fit(time, event, group)
-# Plot
-km.plot(results)
-plt.xlabel("Time (months)")
-plt.ylabel("Survival Rate")
-plt.show()
-
-# interesting variables
-
-# - individualAgeAtCollection => consider how old the person was when they were diagnosed
-# - sex => 
